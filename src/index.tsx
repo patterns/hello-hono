2d128dc2002dd2abbdee03bec7acac5323a09620
興怡 <>
2024-06-02 01:06:13 -0700

    skeleton

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { renderer } from './renderer'
 
 const app = new Hono()
-
 app.use(renderer)
+app.use('/api/*', cors())
+
+
+
+
+
+
+app.onError((err, c) => {
+	console.error(`${err}`);
+	return c.text(err.toString());
+});
+
+app.notFound(c => c.text('Not found', 404));
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+  return c.render(<h1>Hello!</h1><p>This is a placeholder.</p>)
 })
 
 export default app
