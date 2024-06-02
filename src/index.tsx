eafa26d671ce170f0d6dbeac487b06506bc97634
興怡 <>
2024-06-02 01:47:31 -0700

    route placeholders

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,24 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { renderer } from './renderer'
 
-const app = new Hono()
+type Bindings = {
+      DB: D1Database
+}
+
+const app = new Hono<{Bindings: Bindings}>()
 app.use(renderer)
 app.use('/api/*', cors())
 
+app.get('/api/posts/:slug/comments', async c => {
 
 
 
+});
 
+app.post('/api/posts/:slug/comments', async c => {
+
+
+});
 
 app.onError((err, c) => {
 	console.error(`${err}`);
