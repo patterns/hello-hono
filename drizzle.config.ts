befd340a4d53eefa6c552a9ce35ecd6fd515ce25
興怡 <>
2024-06-03 20:20:38 -0700

    switch to gh workflows action

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,35 +1,14 @@
+import { defineConfig } from "drizzle-kit"
 
-import type { Config } from "drizzle-kit";
-
-const {
-	LOCAL_DB_PATH,
-	WRANGLER_CONFIG,
-	DB_NAME = "d1-hello-hono",
-} = process.env;
-
-// Use better-sqlite driver for local development
-export default LOCAL_DB_PATH
-	? ({
-			schema: "./src/schema.ts",
-			driver: "better-sqlite",
-			dbCredentials: {
-				url: LOCAL_DB_PATH,
-			},
-		} satisfies Config)
-	: ({
-			dialect: "sqlite",
-			schema: "./src/schema.ts",
-			out: "./migrations",
-
-			dbCredentials: {
-				wranglerConfigPath:
-					new URL("wrangler.toml", import.meta.url).pathname +
-					// This is a hack to inject additional cli flags to wrangler
-					// since drizzle-kit doesn't support specifying environments
-					WRANGLER_CONFIG
-						? ` ${WRANGLER_CONFIG}`
-						: "",
-				dbName: DB_NAME,
-			},
-		} satisfies Config);
+export default defineConfig({
+	dialect: "sqlite",
+	driver: "d1-http",
+	schema: "./src/schema.ts",
+	out: "./migrations",
+	dbCredentials: {
+		accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+		databaseId: process.env.CLOUDFLARE_DATABASE_ID,
+		token: process.env.CLOUDFLARE_API_TOKEN,
+	},
+})
 
