c327047fb31fed7658c532aacbd831a1c7936b91
興怡 <>
2024-06-07 00:14:25 -0700

    fbauth client id

--- src/client.ts
+++ src/client.ts
@@ -6,20 +6,22 @@ var firebaseui = require('firebaseui');
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-ui.start('#firebaseui-auth-container', {
-
+var uiConfig = {
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
   signInSuccessUrl: 'https://www.adafruit.com/',
   signInOptions: [
     {
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      clientId: process.env.FIREBASE_AUTH_CLIENT_ID,
       scopes: [
         'https://www.googleapis.com/auth/contacts.readonly'
       ],
     }
   ],
+  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+};
 
-});
-
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
 
