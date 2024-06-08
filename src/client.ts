9a0dca101db9e9e4690145dcbafe88e9348a4adc
興怡 <>
2024-06-07 22:18:24 -0700

    attempt verify

--- src/client.ts
+++ src/client.ts
@@ -1,34 +1,6 @@
 
-var firebase = require('firebase');
-var firebaseui = require('firebaseui');
-
-var config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-////  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-};
-firebase.initializeApp(config);
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-var uiConfig = {
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: 'https://www.adafruit.com/',
-  signInOptions: [
-    {
-      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      clientId: process.env.FIREBASE_AUTH_CLIENT_ID,
-      scopes: [
-        'https://www.googleapis.com/auth/contacts.readonly'
-      ],
-    }
-  ],
-  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
-};
-
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+/*
+<script src="https://www.gstatic.com/firebasejs/ui/6.1.0/firebase-ui-auth.js"></script>
+<link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/6.1.0/firebase-ui-auth.css" />
+*/
 
--- src/global.d.ts
+++ src/global.d.ts
@@ -9,6 +9,12 @@ declare module 'hono' {
     Bindings: {
       DB: D1Database
       BEARER_TOKEN: string
+      SERVICE_ACCOUNT_JSON: string
+      PUBLIC_JWK_CACHE_KV: string
+      PUBLIC_JWK_CACHE_KEY: string
+      FIREBASE_PROJECT_ID: string
+      FIREBASE_API_KEY: string
+      FIREBASE_AUTH_DOMAIN: string
     }
   }
 
