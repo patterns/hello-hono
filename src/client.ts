8656accaf9cababc5a309fb1d7deea8418274006
興怡 <549739+patterns@users.noreply.github.com>
2024-06-26 00:24:58 -0700

    Headers (#38)
    * reduced drizzle-orm calls
    
    * func memberByGuid
    
    ---------
    
    Co-authored-by: 興怡 <>

--- src/client.ts
+++
@@ -1,6 +0,0 @@
-
-/*
-<script src="https://www.gstatic.com/firebasejs/ui/6.1.0/firebase-ui-auth.js"></script>
-<link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/6.1.0/firebase-ui-auth.css" />
-*/
-
